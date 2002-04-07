0f591d3288164a1d581b4bd7bce076dd8dfca935
PLT Group <russ@yanofsky.org>
2002-04-07 05:01:26 +0000

    - Pair class used as a hack in some places to squeeze return values from   semantic analysis and code generation into a single object reference.

---
+++ Pair.java
@@ -0,0 +1,19 @@
+//
+//  Pair.java
+//  
+//
+//  Created by tyler on Sat Apr 06 2002.
+//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
+//
+
+public class Pair 
+{
+    public Object right;
+    public Object left;
+    
+    public Pair(Object lt, Object rt)
+	{
+	right = rt;
+	left = lt;
+	}
+}
