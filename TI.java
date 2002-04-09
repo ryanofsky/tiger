c40e4b0da72b2f90bcf3abb3f1c8ab45b23b9530
PLT Group <russ@yanofsky.org>
2002-04-09 01:26:30 +0000

    - added built in function definitions - made use of new RecordInfo.AllocateStack() and RecordInfo.printAll()   functions to start up and print the program

--- TI.java
+++ TI.java
@@ -4,7 +4,7 @@ import Interp.*;
 
 /// Main routine for the Tiger interpreter
 class TI {
-
+    public static boolean BLAH = true;
     public static void main(String[] args) {
 	try {
 	    InputStream i;
@@ -19,23 +19,61 @@ class TI {
 
 	    try {
 
+//Label l = new Label(" print");
+//Statement printFunc = l;
+//printFunc.append( new Sys( Sys. PRINT))
+//         .append( new Rts());
+//Statement s = new Psh( 2);
+//s.append(new Psh( -1));
+//s.append( new Mov( new FrameRel( 0), new StringConstant(" Hello world")))
+// .append( new Jsr( new LabelOperand( l), 0) );
+//s.append( new Mov( new FrameRel( 0), new StringConstant(" This works")))
+// .append( new Jsr( new LabelOperand( l), 0) );
+//
+//s.printAll();
+//printFunc. printAll();
+//s. executeAll( false);
+//if (BLAH) return;
+
+
 		parser.file();
 		LineAST ast = (LineAST)parser.getAST();
 
 		if (ast != null) {
+
+                    //Writer w = new OutputStreamWriter(System.out);
+                    //ast.xmlSerialize(w);
+                    //w.write("\n");
+                    //w.flush();
+
 		    TigerTranslate tt = new TigerTranslate();
 		    RecordInfo r = new RecordInfo("main");
+		    
+                    TigerTranslate.addSystemFunction(r, "print", Sys.PRINT);
+                    TigerTranslate.addSystemFunction(r, "printi", Sys.PRINTI);
+                    TigerTranslate.addSystemFunction(r, "flush", Sys.FLUSH);
+                    TigerTranslate.addSystemFunction(r, "getchar", Sys.GETCHAR);
+                    TigerTranslate.addSystemFunction(r, "ord", Sys.ORD);
+                    TigerTranslate.addSystemFunction(r, "chr", Sys.CHR);
+                    TigerTranslate.addSystemFunction(r, "size", Sys.SIZE);
+                    TigerTranslate.addSystemFunction(r, "substring", Sys.SUBSTRING);
+                    TigerTranslate.addSystemFunction(r, "concat", Sys.CONCAT);
+                    TigerTranslate.addSystemFunction(r, "not", Sys.NOT);
+                    TigerTranslate.addSystemFunction(r, "exit", Sys.EXIT);
+		    
 		    Operand dest = r.newTmp();
 		    tt.expr(ast, dest, r);
 
 		    // Insert a push statement to ensure enough stack space
-		    r.func.insert(new Psh(r.size()));
+		    r.allocateStack();
 
 		    // Print the program
-		    r.func.printAll();
+		    r.printAll();
+		    
+		    System.out.println("--------------------------------------------\nExecuting Program...\n");
 
 		    // Execute the program
-		    r.func.executeAll(true);
+		    r.func.executeAll(false);
 		}
 
 	    } catch (antlr.TokenStreamRecognitionException e) {
