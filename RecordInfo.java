22c53084f5e61d03a63f3a93f4e378abe0337576
PLT Group <russ@yanofsky.org>
2002-04-09 01:24:48 +0000

    - added a new data structure "endTmps" and functions newEndTmp(), newEnd(),   and allocateStack() to manage space at the end of the stack which is used   for function arguments and return values in our calling convention - added a new data structure "functions" to hold references to function   definitions available in the current activation record. added member   functions enterFunc() and printAll() to use the structure.

--- RecordInfo.java
+++ RecordInfo.java
@@ -3,6 +3,8 @@ import Interp.FrameRel;
 import Interp.StackLinks;
 import Interp.Statement;
 import Interp.Label;
+import Interp.Psh;
+import java.util.Vector;
 
 /// Maintains information about what is stored in an activation record
 class RecordInfo {
@@ -16,14 +18,17 @@ class RecordInfo {
     /// Append a statement to the function being built for this record
     public Statement append(Statement s) { return func.append(s); }
 
+    Vector functions = null;
+
     public RecordInfo(String name) {
-	parent = null;
-	func = new Label(name);
+        parent = null;
+        func = new Label(name);
+        functions = new Vector();
     }
 
     public RecordInfo(String name, RecordInfo p) {
-	parent = p;
-	func = new Label(name);
+        parent = p;
+        func = new Label(name);
     }
 
     /*
@@ -51,10 +56,52 @@ class RecordInfo {
 
     /// Return an operand pointing the next available space on the stack
     public Operand newTmp() {
-	FrameRel op = new FrameRel(tos);
-	tos++;
-	if ( tos > max ) max = tos;
-	return op;
+        FrameRel op = new FrameRel(tos);
+        tos++;
+        if ( tos > max ) max = tos;
+        return op;
+    }
+
+    Vector endTmps = new Vector();
+
+    // return an operand that is the end of the stack
+    // (used for function arguments and return values)
+    // the operands will be pointed to the right spots
+    // when the stack is allocated
+    public FrameRel newEndTmp()
+    {
+      FrameRel op = (FrameRel) newTmp();
+      endTmps.add(op);
+      return op;
+    }
+    
+    public void newEnd()
+    {
+      endTmps.add(new Integer(max));
+    }
+
+    public void allocateStack()
+    {
+      func.insert(new Psh(max));
+      
+      int oldMax = 0;
+      
+      for(int i = endTmps.size() - 1; i >= 0; --i)
+      {
+        Object o = endTmps.get(i);
+        if (o instanceof Integer)
+        {
+          oldMax = ((Integer)o).intValue();
+        }
+        else if (o instanceof FrameRel)
+        {
+          FrameRel f = (FrameRel)o;
+          f.offset += max - oldMax;
+        }
+        else // never happens
+          throw new Error("Unrecognized object in endTmps vector");
+        
+      }
     }
 
     /*
@@ -64,23 +111,23 @@ class RecordInfo {
 
     // Represents scopes for variables within this activation record
     public class Scope {
-	Scope parent;
-	java.util.Hashtable dict = new java.util.Hashtable();
+        Scope parent;
+        java.util.Hashtable dict = new java.util.Hashtable();
 
-	public Scope() { parent = null; }
-	public Scope(Scope p) { parent = p; }
+        public Scope() { parent = null; }
+        public Scope(Scope p) { parent = p; }
 
-	/// Locate an identifier in this or an enclosing scope
-	public Object get(String key) {
-	    if ( dict.containsKey(key))
-		return dict.get(key);
-	    else if (parent != null)
-		return parent.get(key);
-	    else return null;
-	}
+        /// Locate an identifier in this or an enclosing scope
+        public Object get(String key) {
+            if ( dict.containsKey(key))
+                return dict.get(key);
+            else if (parent != null)
+                return parent.get(key);
+            else return null;
+        }
 
-	/// Enter an identifier in this scope
-	public void put(String key, Object entry) { dict.put(key,entry); }
+        /// Enter an identifier in this scope
+        public void put(String key, Object entry) { dict.put(key,entry); }
     }
 
     /// Topmost scope in this activation record
@@ -94,56 +141,73 @@ class RecordInfo {
 
     /// Add storage for a variable to this scope
     public Operand newVar(String n) {
-	int varOffset = tos;
-	topScope.put(n, new Integer(varOffset));
-	return newTmp();
+        int varOffset = tos;
+        topScope.put(n, new Integer(varOffset));
+        return newTmp();
     }
 
     /** Return an operand that accesses the given variable while this
-	activation record is active.  This may follow static links, so
-	it's important to use the result immediately. */
+        activation record is active.  This may follow static links, so
+        it's important to use the result immediately. */
     public Operand findVar(String n) {
-	int depth = 0;
-	int offset = 0;
-	RecordInfo ri = this;
-	while (ri != null) {
-	    Object o = ri.topScope.get(n);
-	    if (o != null) {
-		// Located the variable
-		offset = ((Integer)(o)).intValue();
-		break;
-	    }
-	    ++depth; // Didn't find it here, so
-	    ri = ri.parent; // look in the next outermost activation record
-	}
+        int depth = 0;
+        int offset = 0;
+        RecordInfo ri = this;
+        while (ri != null) {
+            Object o = ri.topScope.get(n);
+            if (o != null) {
+                // Located the variable
+                offset = ((Integer)(o)).intValue();
+                break;
+            }
+            ++depth; // Didn't find it here, so
+            ri = ri.parent; // look in the next outermost activation record
+        }
 
-	if ( ri == null ) {
-	    // Should never happen: failure means either static semantics
-	    // weren't checked correctly or that the contents of the
-	    // scopes weren't set up correctly.
-	    // throw new Exception("variable " + n + " not found");
-	}
-	if ( depth == 0 ) return new FrameRel(offset);
-	return new StackLinks(offset, depth);
+        if ( ri == null ) {
+            // Should never happen: failure means either static semantics
+            // weren't checked correctly or that the contents of the
+            // scopes weren't set up correctly.
+            // throw new Exception("variable " + n + " not found");
+        }
+        if ( depth == 0 ) return new FrameRel(offset);
+        return new StackLinks(offset, depth);
     }
 
     /// Enter the definition for a function in the symbol table
-    public void enterFunc(String n, Statement s) { topScope.put(n, s); }
+    public void enterFunc(String n, Statement s)
+    {
+      topScope.put(n, s);
+      RecordInfo ri = this;
+      while(ri.functions == null)
+        ri = ri.parent;
+      ri.functions.add(s);
+    }
+    
+    public void printAll()
+    {
+      for(int i = 0; i < functions.size(); ++i)
+      {
+        Label f = (Label)functions.get(i);
+        f.printAll();
+      }
+      func.printAll(); 
+    }
 
     /// Locate a function in the symbol table by name
     public Statement getFunc(String n) {
-	for ( RecordInfo ri = this ; ri != null ; ri = ri.parent ) {
-	    Object o = ri.topScope.get(n);
-	    if ( o != null ) {
-		// Located the Statement for the function, return it
-		// A type error here means an erroneous program got through;
-		// A function and a variable with the same name should never
-		// be visible in the same scope.
-		return (Statement)o;
-	    }
-	}
-	// Should never happen: indicates a function was not correctly entered
-	// in the symbol tables
-	return null;
+        for ( RecordInfo ri = this ; ri != null ; ri = ri.parent ) {
+            Object o = ri.topScope.get(n);
+            if ( o != null ) {
+                // Located the Statement for the function, return it
+                // A type error here means an erroneous program got through;
+                // A function and a variable with the same name should never
+                // be visible in the same scope.
+                return (Statement)o;
+            }
+        }
+        // Should never happen: indicates a function was not correctly entered
+        // in the symbol tables
+        return null;
     }
 }
