f45b58231aafa11ae8a2a776fca4c173a2f2e08f
PLT Group <russ@yanofsky.org>
2002-05-06 02:00:44 +0000

    - merged in changes from given-hw4 - implemented operand accesses in mips

--- Interp/FrameRel.java
+++ Interp/FrameRel.java
@@ -20,4 +20,14 @@ public class FrameRel extends Operand {
     public void set(Environment e, TigerObj o) throws InterpException {
 	e.stack.set(offset, o);
     }
+
+    public String mipsGet(String reg) {
+	return "  lw " + reg + ", " +
+	    Integer.toString( -4*offset-(offset>=0?12:0)) + "($fp)";
+    }
+
+    public String mipsSet(String reg) {
+	return "  sw " + reg + ", " +
+	    Integer.toString( -4*offset-(offset>=0?12:0)) + "($fp)";
+    }
 }
