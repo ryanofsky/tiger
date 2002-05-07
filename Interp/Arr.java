fc9001ba4ad90b51f8a83f83437b9c23886ea702
PLT Group <russ@yanofsky.org>
2002-05-07 03:59:44 +0000

    - changed to work with different sized array elements

--- Interp/Arr.java
+++ Interp/Arr.java
@@ -5,11 +5,13 @@ public class Arr extends Statement {
     Operand dest;
     Operand size;
     Operand src;
+    int intSize;
 
-    public Arr(Operand d, Operand s, Operand sr) {
+    public Arr(Operand d, Operand s, Operand sr, int recordSize) {
         dest = d;
         size = s;
         src = sr;
+	intSize = recordSize;
     }
 
     public String string() { return "  arr " + dest.string() + ", " +
@@ -26,11 +28,16 @@ public class Arr extends Statement {
     public String mips()
     {
       
-      int sizeOfRec = 4;
+      int sizeOfRec = intSize;
+      int increment = sizeOfRec;
+      
+      if(increment == 0)
+	{increment = 4;}
+      
       StringBuffer output = new StringBuffer("# Arr::mips() begin\n");
 
       output.append(size.mipsGet("$t7") + "\n");
-      output.append("addi $t6, $zero, " + sizeOfRec + "\n"); // $t6 = sizeOfRec
+      output.append("addi $t6, $zero, " + increment + "\n"); // $t6 = sizeOfRec
       
       output.append("mul $a0, $t7, $t6\n"); // find total length
       output.append("li $v0, 9\n"); // 9 == sbrk
@@ -45,6 +52,9 @@ public class Arr extends Statement {
       
       output.append("move $t0, $v0\n"); // outer loop through array
       output.append(top.mips());
+      
+      if(sizeOfRec != 0)
+	{
         output.append("move $t1, $zero\n"); // inner loop through words of rec
         output.append(copyTop.mips());
 	
@@ -55,15 +65,21 @@ public class Arr extends Statement {
         output.append("addi $v0, $v0, " + sizeOfRec + "\n");
 	
 	output.append("add $s0, $t1, $t0\n");
-	output.append("lw $t2, 0($s0)\n");
+	output.append("sw $t2, 0($s0)\n");
 	
         //output.append("sw $t2, $t1($t0)\n");
         output.append("blt $t1, $t6, " + copyTop.mipsName() + "\n");
-      output.append("addi $t0, $t0, " + sizeOfRec + "\n");
+	}
+    else
+	{output.append("sw $t4, 0($t0)\n");}
+	
+      output.append("addi $t0, $t0, " + increment + "\n");
       output.append("blt $t0, $t7, " + top.mipsName() + "\n");
 
-      output.append(dest.mipsGet("$t0") + "\n");
-      output.append("sw $v0, 0($t0)"); // save pointer
+	output.append("#Type of operand: " + dest + "\n");
+
+      output.append(dest.mipsSet("$v0") + "\n");
+      //output.append("sw $v0, 0($t0)"); // save pointer
 
       return output.toString();
     }
