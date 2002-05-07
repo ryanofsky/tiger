3ba571123b16d290594aad31c1a364d5e20c6a99
PLT Group <russ@yanofsky.org>
2002-05-07 05:08:40 +0000

    - aesthetic changes, same functionality

--- Interp/Ent.java
+++ Interp/Ent.java
@@ -1,14 +1,24 @@
 package Interp;
 
 public class Ent extends Statement {
-    public Ent() {}
+    public Ent() {
+    
+    /*
+    try
+	{throw new Exception("Tracing.");}
+    catch(Exception e)
+	{e.printStackTrace();}
+    */
+    
+    }
 
     public String string() { return "  ent"; }
 
     /// Do nothing
     public Statement execute(Environment e) { return next; }
 
-    public String mips() {
+    public String mips() 
+    {
 	return
 	    "  sw  $ra, 0($sp)  # Save return address\n" +
 	    "  sw  $a0, -4($sp) # Initialize static link\n" +
