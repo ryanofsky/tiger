2e00a63bdf2d8fd7f9098e0e5cc1abda1cdfbf20
PLT Group <russ@yanofsky.org>
2002-05-07 01:36:54 +0000

    - moved builtin function declarations from TC.java to TigerTranslate.g

--- TC.java
+++ TC.java
@@ -6,65 +6,56 @@ import Interp.*;
 class TC {
     public static boolean BLAH = true;
     public static void main(String[] args) {
-	try {
-	    InputStream i;
-	    if (args.length == 0) {
-		i = new DataInputStream(System.in);
-	    } else {
-		i = new FileInputStream(args[0]);
-	    }
-	    TigerLexer lexer = new TigerLexer(i);
-	    TigerParser parser = new TigerParser(lexer);
-	    parser.setASTNodeClass("Semant.LineAST");
+        try {
+            InputStream i;
+            if (args.length == 0) {
+                i = new DataInputStream(System.in);
+            } else {
+                i = new FileInputStream(args[0]);
+            }
+            TigerLexer lexer = new TigerLexer(i);
+            TigerParser parser = new TigerParser(lexer);
+            parser.setASTNodeClass("Semant.LineAST");
 
-	    try {
+            try {
 
-		parser.file();
-		LineAST ast = (LineAST)parser.getAST();
+                parser.file();
+                LineAST ast = (LineAST)parser.getAST();
 
-		if (ast != null) {
-		    TigerTranslate tt = new TigerTranslate();
-		    RecordInfo r = new RecordInfo("main");
-		    
+                if (ast != null) {
+                    TigerTranslate tt = new TigerTranslate();
+                    RecordInfo r = new RecordInfo("main");
+                    tt.addSystemFunctions(r);
+                    
+                    Operand dest = r.newTmp();
+                    tt.expr(ast, dest, r);
+                    r.append(new Rts());
 
-		    TigerTranslate.addSystemFunction(r, "print", Sys.PRINT);
-                    TigerTranslate.addSystemFunction(r, "printi", Sys.PRINTI);
-                    TigerTranslate.addSystemFunction(r, "flush", Sys.FLUSH);
-                    TigerTranslate.addSystemFunction(r, "getchar", Sys.GETCHAR);
-                    TigerTranslate.addSystemFunction(r, "ord", Sys.ORD);
-                    TigerTranslate.addSystemFunction(r, "chr", Sys.CHR);
-                    TigerTranslate.addSystemFunction(r, "size", Sys.SIZE);
-                    TigerTranslate.addSystemFunction(r, "substring", Sys.SUBSTRING);
-                    TigerTranslate.addSystemFunction(r, "concat", Sys.CONCAT);
-                    TigerTranslate.addSystemFunction(r, "not", Sys.NOT);
-                    TigerTranslate.addSystemFunction(r, "exit", Sys.EXIT);
+                    // Insert a push statement to ensure enough stack space
+                    r.allocateStack();
 
-		    
-		    
-		    Operand dest = r.newTmp();
-		    tt.expr(ast, dest, r);
-		    r.append(new Rts());
+                    for(int x = 0; x < r.functions.size(); ++x )
+                    {
+                      Label f = (Label)r.functions.get(x);
+                      f.printMips();
+                    }
 
-		    // Insert a push statement to ensure enough stack space
-		    r.allocateStack();
+                    // Print MIPS assembly
+                    r.func.printMips();
 
+                }
 
-		    // Print MIPS assembly
-		    r.func.printMips();
+            } catch (antlr.TokenStreamRecognitionException e) {
+                System.err.println( lexer.getLine() + ":" + e );
+                System.exit(1);
+            } catch (antlr.RecognitionException e) {
+                System.err.println( e.toString() );
+                System.exit(1);
+            }
 
-		}
-
-	    } catch (antlr.TokenStreamRecognitionException e) {
-		System.err.println( lexer.getLine() + ":" + e );
-		System.exit(1);
-	    } catch (antlr.RecognitionException e) {
-		System.err.println( e.toString() );
-		System.exit(1);
-	    }
-
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    System.err.println("exception: " + e);
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("exception: " + e);
+        }
     }
 }
